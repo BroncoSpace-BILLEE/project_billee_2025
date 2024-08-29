f10907f6b6b5cfb7e961cf7cf085ac6b276d7696
Sam Ly <sam@samly.me>
2024-08-29 11:56:30 -0700

    Feature/urdf example (#5)
    * add urdf_example package
    
    * update some stuff

---
+++ ros2_ws/build/urdf_example/CMakeFiles/urdf_example_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/urdf_example_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/urdf_example_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
