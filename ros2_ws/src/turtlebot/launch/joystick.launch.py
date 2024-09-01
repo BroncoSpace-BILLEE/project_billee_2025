76116289cf1482c1d19a91b35e81825cfc13d722
Sam Ly <sam@samly.me>
2024-09-01 14:45:05 -0700

    Feature/turtlebot (#11)
    * add turtlebot package
    
    * add urdf description
    
    * fix typo in world chassis
    
    * rename variable
    
    * add gazebo file
    
    * add gazebo diff drive
    
    * save rviz2 config
    
    * implement joystick controls to simulation

---
+++ ros2_ws/src/turtlebot/launch/joystick.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    joy_params = os.path.join(get_package_share_directory('turtlebot'),'config','joystick.yaml')
+
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params],
+         )
+
+    teleop_node = Node(
+            package='teleop_twist_joy', 
+            executable='teleop_node',
+            name = 'teleop_node',
+            parameters=[joy_params],
+            )
+
+    return LaunchDescription([
+        joy_node,
+        teleop_node,
+    ])
